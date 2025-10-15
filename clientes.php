4f9245a25442d66a8f4b6efc873274a132a3dd18
debrilem <debma010@gmail.com>
2025-10-15 00:02:28 -0600

    Creación de Proyecto

---
+++ clientes.php
@@ -0,0 +1,5 @@
+<?php require_once "./vistas/vista_superior.php" ?>
+
+Clientes
+
+<?php require_once "./vistas/vista_inferior.php" ?>
---
+++ vistas/vista_superior.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1" content="Proyecto Universitario">
+    <title>Tercera 1</title>
+    <link rel="styleheet" href="./css/main.css">
+
+
+
+
+
+</head>
